446cb24a7bcdd2fb4b4a492920eec98e114e2cf1
Susan Novalinski Hung <snovalinski@gmail.com>
2021-06-08 18:44:38 -0300

    integrado el navbar

---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contactame</li>
+                <li>FAQ</li>
+                <li>Acerca de mi</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
+
---
+++ server.js
@@ -0,0 +1,1 @@
+console.log("Hey, that's start script");
